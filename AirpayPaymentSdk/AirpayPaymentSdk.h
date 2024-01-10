051f14705bc03b0e04a5095b30f6a9a6884271ef
Tonny Franzis <tonny.franzis@airpay.co.in>
2024-01-10 18:08:14 +0530

    Initial Commit

---
+++ AirpayPaymentSdk/AirpayPaymentSdk.h
@@ -0,0 +1,18 @@
+//
+//  AirpayPaymentSdk.h
+//  AirpayPaymentSdk
+//
+//  Created by Tonny Franzis on 10/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AirpayPaymentSdk.
+FOUNDATION_EXPORT double AirpayPaymentSdkVersionNumber;
+
+//! Project version string for AirpayPaymentSdk.
+FOUNDATION_EXPORT const unsigned char AirpayPaymentSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirpayPaymentSdk/PublicHeader.h>
+
+
